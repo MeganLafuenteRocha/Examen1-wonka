da937560dbe7dc7d4e2a8c0c650ad697c57110cb
Megan Lafuente Rocha <andreameganlafuenterocha@gmail.com>
2025-08-03 22:11:48 -0400

    crear data de seeder de Marca

--- database/seeders/MarcaSeeder.php
+++ database/seeders/MarcaSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Marca;
+
 
 class MarcaSeeder extends Seeder
 {
@@ -12,6 +14,31 @@ class MarcaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $marcas = [
+            [
+                'nombre' => 'Chocolates Premium',
+                'descripcion' => 'Línea de chocolates gourmet con ingredientes de la más alta calidad.'
+            ],
+            [
+                'nombre' => 'Dulces Mágicos',
+                'descripcion' => 'Categoría de dulces con efectos especiales y sabores únicos.'
+            ],
+            [
+                'nombre' => 'Caramelos Clásicos',
+                'descripcion' => 'Tradicionales caramelos y gomitas de toda la vida con el toque Wonka.'
+            ],
+            [
+                'nombre' => 'Bebidas Fantásticas',
+                'descripcion' => 'Línea de bebidas gaseosas y líquidos con propiedades especiales.'
+            ],
+            [
+                'nombre' => 'Edición Limitada',
+                'descripcion' => 'Productos exclusivos y de colección con disponibilidad limitada.'
+            ]
+        ];
+
+        foreach ($marcas as $marca) {
+            Marca::create($marca);
+        }
     }
 }
